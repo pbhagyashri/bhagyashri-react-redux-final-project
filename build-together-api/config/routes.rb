c828aa0fb2a3634f7661e187aefd32e571018837
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-04-13 01:39:30 -0400

    Add routes for users, projects and login, logout, signup to routes.rb

--- build-together-api/config/routes.rb
+++ build-together-api/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :users, only: [:edit, :update, :destroy, :show, :index]
+
+    resources :projects
+
+    post "/signup", to: "users#signup"
+    post "/login", to: "sessions#login"
+    get "logout", to: "sessions#destroy"
+  end
 end
