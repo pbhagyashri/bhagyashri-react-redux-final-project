6f39f6b995f33df3a61c8b6a767430f3579656da
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-04-13 01:34:08 -0400

    Add index , create, show, edit, update and destroy actions and set_project and project_params helper methods to projects_controller

---
+++ build-together-api/app/controllers/api/projects_controller.rb
@@ -0,0 +1,52 @@
+class Api::ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    render json: Project.all
+  end
+
+  def show
+    render json: @project
+  end
+
+  def create
+    project = Project.new(project_params)
+
+    if project.save
+      render json: project
+    else
+      render json: {message: project.errors}, status: 400
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      render json: @project
+    else
+      render json: {message: @project.errors}, status: 400
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      render status: 204
+    else
+      render json: {message: "unable to process your request"}, status: 400
+    end
+  end
+
+  private 
+
+  def project_params
+    params.require(:project).permit(:name, :languages, :description, :duration)
+  end
+
+  def set_project
+    @project = Project.find_by(id: params[:id])
+  end
+
+
+end
