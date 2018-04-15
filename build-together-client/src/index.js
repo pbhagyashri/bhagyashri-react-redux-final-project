822edcf9dc277437fd6fa2b16ec4c6e134b4a058
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-04-15 15:52:08 -0400

    install and save redux and react-redux libraries and Add actions and reducers folders to src

--- build-together-client/src/index.js
+++ build-together-client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Redux from 'redux'
 import './index.css';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
