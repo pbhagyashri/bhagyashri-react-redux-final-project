c28c8837ba5c0951e1f33c10c5db9d6afd6d5ae6
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-05-07 13:38:38 -0400

    Run rake db:migrate to add migration for Comment. Update schema.rb

--- build-together-api/db/schema.rb
+++ build-together-api/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180428060148) do
+ActiveRecord::Schema.define(version: 20180507171243) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
